16adacccc5ca9f01a3084f4f8b58f545c712b006
codemeron1 <wondelrosario@gmail.com>
2024-11-08 18:42:12 +0800

    for quiz

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 const express = require("express");
-const cors = require('cors');  // Import the CORS package
 const app = express();  
 
 app.use(express.json());
-// Allow requests from specific origins, in your case, http://127.0.0.1:5500
-// app.use(cors({
-//     origin: 'http://127.0.0.1:5500',  // Replace with your frontend's address
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowable HTTP methods
-//     credentials: true // If your API requires cookies/auth headers
-// }));
-
-// Allow all origins
-app.use(cors());
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-
-app.post("/", (request, response) => {
-    const status = {
-        "Status": "Running"
-    };
-
-    response.send(status);
+//callback function: javascript
+app.post("/languages", (request, response) => {
+    response.send("hello");
 });
+
+
